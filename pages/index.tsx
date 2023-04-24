63f5dd207a9c2bbca289295171152b3b5faee65f
Gabriel Fragoso <gabriel.fragoso@lab510.br>
2023-04-24 10:02:39 -0300

    fix: remove scrollama

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,51 +4,15 @@ import Clientes from '@/components/Clientes'
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import Trabalho from '@/components/Trabalho'
-
-import { Scrollama, Step } from "react-scrollama";
-import { useState } from "react";
-
-
 export default function Home() {
-  const [currentStepIndex, setCurrentStepIndex] = useState(0);
-
-  const handleStepEnter = ({ data }: any) => {
-    setCurrentStepIndex(data);
-  };
-
-  const steps = [
-    {
-      id: "step1",
-      content: <Apresentação id='apresentacao' />,
-    },
-    {
-      id: "step2",
-      content: <Clientes id="clientes" />,
-    },
-    {
-      id: "step3",
-      content: <Trabalho id="trabalho" />,
-    },
-    {
-      id: "step4",
-      content: <About id="sobre-mim" />,
-    },
-  ];
 
   return (
     <>
       <Header />
-      <Scrollama onStepEnter={handleStepEnter}>
-        {steps.map((step, index) => (
-          <Step key={step.id} data={index}>
-            <div
-              className={`item ${index === currentStepIndex ? "is-active" : ""}`}
-            >
-              {step.content}
-            </div>
-          </Step>
-        ))}
-      </Scrollama>
+      <Apresentação id='apresentacao' />
+      <Clientes id="clientes" />
+      <Trabalho id="trabalho" />
+      <About id="sobre-mim" />
       <Footer />
     </>
   )
